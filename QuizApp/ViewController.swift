03d1b58a41d2269bb8f883e026771bbde7d3faca
Muammer <muammerakca10@gmail.com>
2023-01-27 14:18:04 +0300

    Project Created and Storyboard designed

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Muammer on 27.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func answerTapped(_ sender: Any) {
+        
+        
+    }
+    
+}
+
